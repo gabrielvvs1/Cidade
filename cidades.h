ee1c8092c394981e00189667265cfda207f2135b
gabrielvvs1 <87244095+gabrielvvs1@users.noreply.github.com>
2024-06-12 20:00:42 -0300

    cidades.h

--- cidades.h
+++ cidades.h
@@ -1,20 +1,21 @@
 #ifndef CIDADES_H
 #define CIDADES_H
 
-typedef struct {
-    char Nome[256]; // Nome do cidade
-    int Posicao; // Posição da cidade
-    } Cidade;
+typedef struct
+{
+    char Nome[256]; // Nome da cidade
+    int Posicao;    // Posição da cidade
+} Cidade;
 
-typedef struct {
-    int N; // Número de cidades
-    int T; // Comprimento da estrada
+typedef struct
+{
+    int N;     // Número de cidades
+    int T;     // Comprimento da estrada
     Cidade *C; // Vetor de cidades
-    } Estrada;
+} Estrada;
 
-
-Estrada *getEstrada(const char *nomeArquivo); // Inicializa cidades no TAD indicado acima.
-double calcularMenorVizinhanca(const char *nomeArquivo); // Retorna a menor vizinhança.
-char *cidadeMenorVizinhanca(const char *nomeArquivo); // Retorna a cidade que tem menor vizinhança.
+Estrada *getEstrada(const char *nomeArquivo);            // Inicializa cidades no TAD indicado acima
+double calcularMenorVizinhanca(const char *nomeArquivo); // Retorna a menor vizinhança
+char *cidadeMenorVizinhanca(const char *nomeArquivo);    // Retorna a cidade que tem menor vizinhança
 
 #endif
